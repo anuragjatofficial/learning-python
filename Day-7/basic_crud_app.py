42062e085b7db26801f2a0adce0747cac4c822c2
anuragjatofficial <anurag.1101140@gmail.com>
2023-11-15 00:08:42 +0530

    added basic CRUD app

--- Day-7/basic_crud_app.py
+++ Day-7/basic_crud_app.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/create')
+data_dict = {}
+
+
+@app.route("/create", methods=["POST"])
 def create():
-    
+    if request.method == "POST":
+        new_entry = request.get_json()
+        data_dict[new_entry.get("id")] = new_entry
+        return "created succesfully"
+
+
+@app.route("/get")
+def get():
+    return data_dict
+
+
+@app.route("/update/<int:id>", methods=["PUT"])
+def update(id):
+    if request.method == "PUT":
+        updated_entry = request.get_json()
+        data_dict[id] = updated_entry
+        return "entry updated succesfully"
+
+
+@app.route("/delete/<int:id>", methods=["DELETE"])
+def delete(id):
+    if request.method == "DELETE":
+        del data_dict[id]
+        return f"deleted {id} entry succesfully "
+
+
+if __name__ == "__main__":
+    app.run()
